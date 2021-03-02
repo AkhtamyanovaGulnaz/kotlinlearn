4216ce2f391ee1007aeb0e955e3a418e97bad403
aastraeu <gulnaz_gub@mail.ru>
2021-03-03 00:07:23 +0300

    3 commit

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyC6ryMWPQl96jf2pGG5RseWb5BK9reKdzk',
+  authDomain: 'learnkotlin-9c03c.firebaseapp.com',
+  projectId: 'learnkotlin-9c03c',
+  storageBucket: 'learnkotlin-9c03c.appspot.com',
+  messagingSenderId: '405693990457',
+  appId: '1:405693990457:web:78ca37c0e04153c4d42fe8',
+  measurementId: 'G-QW3G9BJGHW',
+}
