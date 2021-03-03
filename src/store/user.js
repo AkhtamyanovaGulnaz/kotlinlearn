b1be0f6f05ef9a42397be3272511bdf9822904ea
aastraeu <gulnaz_gub@mail.ru>
2021-03-03 12:44:45 +0300

    3 commit

--- src/store/user.js
+++ src/store/user.js
@@ -28,22 +28,22 @@ export default {
           commit('SET_ERROR', error.message)
         })
     },
-    SIGNIN({ commit }, payload) {
-      commit('SET_PROCESSING', true)
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(payload.email, payload.password)
-        .then((user) => {
-          commit('SET_USER', user.uid)
-          commit('SET_PROCESSING', false)
-        })
-        .catch(function(error) {
-          commit('SET_PROCESSING', false)
-          commit('SET_ERROR', error.message)
-        })
-    },
-    getters: {
-      isUserAuthenticated: (state) => state.user.isAuthenticated,
-    },
+  },
+  SIGNIN({ commit }, payload) {
+    commit('SET_PROCESSING', true)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(payload.email, payload.password)
+      .then((user) => {
+        commit('SET_USER', user.uid)
+        commit('SET_PROCESSING', false)
+      })
+      .catch(function(error) {
+        commit('SET_PROCESSING', false)
+        commit('SET_ERROR', error.message)
+      })
+  },
+  getters: {
+    isUserAuthenticated: (state) => state.user.isAuthenticated,
   },
 }
